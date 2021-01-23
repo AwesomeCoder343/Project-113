378f357c6f0a246ac2fff83b0375c449257d9179
AwesomeCoder343 <73550340+AwesomeCoder343@users.noreply.github.com>
2021-01-22 19:26:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+}
+function draw(){
+
+    /*circle(50, 50, 250);*/
+ stroke("red");
+ strokeWeight(25);
+    rect(-10,-10,350,350);
+    stroke("green");
+    strokeWeight(10);
+    circle(2,2,80);  
+    circle(2,280,80);  
+    circle(298,2,80);  
+    circle(298,280,80);  
+   }
+ 
